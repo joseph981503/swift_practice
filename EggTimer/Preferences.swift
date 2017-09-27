f8c7e3edbebd34185cf97bf4d0528660089e00f8
joseph981503 <31875062+joseph981503@users.noreply.github.com>
2017-09-27 21:41:12 +0800

    EggTimer

---
+++ EggTimer/Preferences.swift
@@ -0,0 +1,20 @@
+
+
+import Foundation
+
+struct Preferences {
+
+  var selectedTime: TimeInterval {
+    get {
+      let savedTime = UserDefaults.standard.double(forKey: "selectedTime")
+      if savedTime > 0 {
+        return savedTime
+      }
+      return 360
+    }
+    set {
+      UserDefaults.standard.set(newValue, forKey: "selectedTime")
+    }
+  }
+
+}
